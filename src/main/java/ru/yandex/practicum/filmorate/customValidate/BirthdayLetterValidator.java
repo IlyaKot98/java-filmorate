85776bb09e31f682e682b240bd66aa69d917db8e
Ilya Kotkov <kotkov-i@mail.ru>
2024-10-29 01:00:32 +0300

    feat: Копирование ветки controllers-films-users

---
+++ src/main/java/ru/yandex/practicum/filmorate/customValidate/BirthdayLetterValidator.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.customValidate;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class BirthdayLetterValidator implements ConstraintValidator<BirthdayLetter, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+        if (value.isBefore(LocalDate.of(1895,12,28))) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,22 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-/**
- * Film.
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Film {
+public class User {
     private Integer id;
-    @NotBlank private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private int duration;
+    @NotBlank private String email;
+    @NotBlank private String login;
+    private String name;
+    @NotNull private LocalDate birthday;
 }
